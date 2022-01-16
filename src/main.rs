71bd5cfb587c7a270413dc87eeaab31dd0878c6b
Sergey Bargamon <sergey@bargamon.ru>
2022-01-16 12:41:04 +0100

    readme

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ fn main() {
     let read = Timer::new("read");
     if let Ok(d) = Dict::from_file("./data/sowpods_5.txt") {
         read.end();
-        let f2 = Timer::new("freq");
-        let freq2 = d.get_char_freq();
+        let f2 = Timer::new("weights");
+        let weights = d.get_char_weights();
         f2.end();
         // println!("{:?}", freq2);
         let filter = Timer::new("filters");
@@ -20,7 +20,7 @@ fn main() {
             .apply();
         filter.end();
         let mc = Timer::new("most_common");
-        println!("{:#?}", words.most_common(&freq2, 10));
+        println!("{:#?}", words.most_common(&weights, 10));
         mc.end();
     }
     whole.end();
@@ -28,19 +28,19 @@ fn main() {
 
 struct Timer<'a> {
     start: std::time::Instant,
-    t: &'a str,
+    name: &'a str,
 }
 
 impl<'a> Timer<'a> {
-    pub fn new(t: &'a str) -> Self {
-        return Self {
+    pub fn new(name: &'a str) -> Self {
+        Self {
             start: Instant::now(),
-            t: t,
-        };
+            name,
+        }
     }
 
     pub fn end(self) {
         let duration = self.start.elapsed();
-        println!("{} is done in: {:?}", self.t, duration);
+        println!("{} is done in: {:?}", self.name, duration);
     }
 }
