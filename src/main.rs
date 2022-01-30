afe349cf2f7c2a236ee5038a2835b36c2166c25f
Sergey Bargamon <sergey@bargamon.ru>
2022-01-30 23:37:39 +0100

    new thing version. sorted dict

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,27 @@
-use a_thing::dict::Dict;
+use a_thing::file::from_file;
+use a_thing::filters::contains_chars;
 
+use std::io::Error;
 use std::time::Instant;
 
-fn main() {
+fn main() -> Result<(), Error> {
     let whole = Timer::new("whole");
     let read = Timer::new("read");
-    if let Ok(d) = Dict::from_file("./data/sowpods_5.txt") {
+    if let Ok(d) = from_file("./data/sowpods_5.txt") {
         read.end();
-        let f2 = Timer::new("weights");
-        let weights = d.get_char_weights();
-        f2.end();
-        // println!("{:?}", freq2);
         let filter = Timer::new("filters");
-        let words = d
-            .contains_chars("")
+        let words = contains_chars(&d, "")
             .not_contains_chars("")
             .positional_contains_chars(&[None, None, None, None, None])
             .positional_not_contains_chars(&[None, None, None, None, None])
             .apply();
+
         filter.end();
-        let mc = Timer::new("most_common");
-        println!("{:#?}", words.most_common(&weights, 10));
-        mc.end();
+
+        println!("{:#?}", words.iter().take(10).collect::<Vec<&String>>());
     }
     whole.end();
+    Ok(())
 }
 
 struct Timer<'a> {
@@ -41,6 +39,6 @@ impl<'a> Timer<'a> {
 
     pub fn end(self) {
         let duration = self.start.elapsed();
-        println!("{} is done in: {:?}", self.name, duration);
+        println!("{}: {:?}", self.name, duration);
     }
 }
