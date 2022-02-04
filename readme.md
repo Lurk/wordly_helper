1da46da6de54144935bfc32deb7fad86bef3e8a3
Sergey Bargamon <sergey@bargamon.ru>
2022-02-04 22:52:24 +0100

    api

--- readme.md
+++ readme.md
@@ -1,6 +1,22 @@
-# [Wordle](https://www.powerlanguage.co.uk/wordle/) game cheat
+# [Wordle](https://www.powerlanguage.co.uk/wordle/) game cheat api
 
-![wordle cheat](https://res.cloudinary.com/barhamon/image/upload/v1643662606/github/Wordle.jpg)
+the dictionary is
+from [guess_my_word repo](https://github.com/powerlanguage/guess-my-word/blob/master/wordlist/sowpods.txt)
 
-the dictionary is from [guess_my_word repo](https://github.com/powerlanguage/guess-my-word/blob/master/wordlist/sowpods.txt)
+## API
 
+### [POST] /api/word
+
+json body
+
+```rust 
+struct Rules {
+  contains: String,
+  not_contains: String,
+  positional_contains: String,
+  positional_not_contains: Vec<String>,
+}
+```
+where:
+* positional_contains is string with unknown letters marked as "_"
+* positional_not_contains is array of strings where unknown letters marked as "_"
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,58 @@
 use a_thing::file::from_file;
 use a_thing::filters::contains_chars;
-
-use std::io::Error;
+use actix_cors::Cors;
+use actix_web::{web, App, HttpServer, Result};
+use chrono::prelude::*;
+use serde::Deserialize;
 use std::time::Instant;
 
-fn main() -> Result<(), Error> {
-    let whole = Timer::new("whole");
-    let read = Timer::new("read");
-    if let Ok(d) = from_file("./data/sowpods_5.txt") {
-        read.end();
-        let filter = Timer::new("filters");
-        let words = contains_chars(&d, "")
-            .not_contains_chars("")
-            .positional_contains_chars(&[None, None, None, None, None])
-            .positional_not_contains_chars(&[None, None, None, None, None])
-            .take(10)
-            .apply();
-
-        filter.end();
-
-        println!("{:#?}", words);
-    }
-    whole.end();
-    Ok(())
+#[derive(Deserialize, Debug)]
+struct Rules {
+  contains: String,
+  not_contains: String,
+  positional_contains: String,
+  positional_not_contains: Vec<String>,
 }
 
-struct Timer<'a> {
-    start: std::time::Instant,
-    name: &'a str,
+fn positional_string_to_vec(str: &str) -> Vec<Option<char>> {
+  str.chars()
+    .into_iter()
+    .map(|char| if char == '_' { None } else { Some(char) })
+    .collect()
 }
 
-impl<'a> Timer<'a> {
-    pub fn new(name: &'a str) -> Self {
-        Self {
-            start: Instant::now(),
-            name,
-        }
-    }
+async fn word(rules: web::Json<Rules>, dict: web::Data<Vec<String>>) -> Result<String> {
+  let start = Instant::now();
+  let positional_contains = positional_string_to_vec(&rules.positional_contains);
+  let mut v: Vec<Vec<Option<char>>> = vec![];
+  let mut filters = contains_chars(&dict, rules.contains.as_str())
+    .not_contains_chars(rules.not_contains.as_str())
+    .positional_contains_chars(&positional_contains);
 
-    pub fn end(self) {
-        let duration = self.start.elapsed();
-        println!("{}: {:?}", self.name, duration);
-    }
+  for pc in &rules.positional_not_contains {
+    v.push(positional_string_to_vec(pc));
+  }
+  for pc in v.iter() {
+    filters = filters.positional_not_contains_chars(pc)
+  }
+  let words = filters.take(1).apply();
+
+  println!("[{}] GET word: {:?}", Utc::now(), start.elapsed());
+  Ok(words.get(0).unwrap_or(&"".to_string()).clone())
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+  let d = from_file("./data/sowpods_5.txt")?;
+  HttpServer::new(move || {
+    let cors = Cors::permissive();
+
+    App::new()
+      .wrap(cors)
+      .data(d.clone())
+      .route("/api/word", web::post().to(word))
+  })
+    .bind("0.0.0.0:80")?
+    .run()
+    .await
 }
